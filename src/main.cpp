b8a0de1bfd0e24443ca5618135b2de708865c5c8
Игорь Антипов <igorantivirus@yandex.ru>
2025-10-20 23:29:32 +0700

    Имя главного конфига подтягивается из консоли

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,15 @@ const std::string TgBot::InlineQueryResultArticle::TYPE = "article";
 const std::string TgBot::InputTextMessageContent::TYPE = "text";
 #endif
 
-int main()
+int main(int argc, char** argv)
 {
 #if defined(_WIN32) || defined(_WIN64)
     std::system("chcp 65001 > nul");
 #endif
+    if(argc != 2)
+        return 1;
 
-    Services::initInstance("mainConfig.json");
+    Services::initInstance(argv[0]);
 
     Bot bot(service::config().tgApiKey);
     bot.run();
