8bbc9713fb6982d3ac83616f1eecb677968aae6b
Игорь Антипов <igorantivirus@yandex.ru>
2025-10-21 00:05:02 +0700

    Переход на старую версию получения времени (без format)

--- src/ServiceLocator/Logger.hpp
+++ src/ServiceLocator/Logger.hpp
@@ -5,6 +5,10 @@
 #include <fstream>
 #include <iostream>
 #include <chrono>
+// #include <format>
+#include <sstream>
+#include <iomanip>
+#include <ctime>
 
 enum class LogLevel : unsigned char
 {
@@ -121,29 +125,29 @@ private: // static
         return converter_[static_cast<unsigned char>(level)];
     }
 
-    // static std::string getCurentTime()
-    // {
-    //     using namespace std::chrono;
-
-    //     auto now = system_clock::now();
-    //     auto ms = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
-
-    //     // Преобразуем в time_t для strftime/put_time
-    //     std::time_t t = system_clock::to_time_t(now);
-    //     std::tm tm = *std::localtime(&t);
-
-    //     std::ostringstream oss;
-    //     oss << "("
-    //         << std::put_time(&tm, "%d-%m-%Y %X")  // формат даты и времени
-    //         << ":" << std::setw(3) << std::setfill('0') << ms.count()  // миллисекунды
-    //         << ")";
-    //     return oss.str();
-    // }
-
     static std::string getCurentTime()
     {
-        auto now = std::chrono::system_clock::now();
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
-        return std::format("({:%d-%m-%Y %X}:{:03d})", now, ms.count());
+        using namespace std::chrono;
+
+        auto now = system_clock::now();
+        auto ms = duration_cast<milliseconds>(now.time_since_epoch()) % 1000;
+
+        // Преобразуем в time_t для strftime/put_time
+        std::time_t t = system_clock::to_time_t(now);
+        std::tm tm = *std::localtime(&t);
+
+        std::ostringstream oss;
+        oss << "("
+            << std::put_time(&tm, "%d-%m-%Y %X")  // формат даты и времени
+            << ":" << std::setw(3) << std::setfill('0') << ms.count()  // миллисекунды
+            << ")";
+        return oss.str();
     }
+
+    // static std::string getCurentTime()
+    // {
+    //     auto now = std::chrono::system_clock::now();
+    //     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+    //     return std::format("({:%d-%m-%Y %X}:{:03d})", now, ms.count());
+    // }
 };
